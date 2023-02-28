42ff09ed3d2fb40e512c3ffc46e51d8eac32f1f8
gaz1697 <gazlan111@gmail.com>
2023-02-28 16:21:22 +0300

    ..

--- migrations/sqls/20230228114248-structure-up.sql
+++ migrations/sqls/20230228114248-structure-up.sql
@@ -16,6 +16,8 @@ create table orders (
     id int not null,
     user_id int not null,
     product_id int not null,
+    quantity int not null,
+    status varchar(255) not null,
     primary key (id, product_id),
     foreign key (user_id) references user_info(id),
     foreign key (product_id) references products(id)
